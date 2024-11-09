1e4c11e5af631c18a8dc5ab16dfedd9f94e4d2dd
amelweb <amelwebhost@gmail.com>
2024-11-10 00:37:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,13 +3,19 @@ const express = require('express');
 const app = express();
 
 app.use((req, res, next) => {
-  // Get the IP address of the client
-  const ip = req.headers['x-forwarded-for'] || req.ip;
+    // Extract the full list of IPs from the 'x-forwarded-for' header
+    const ipList = req.headers['x-forwarded-for'] || '';
+    
+    // Get the actual client IP (first IP in the list)
+    const actualIP = ipList.split(',')[0].trim() || req.ip;
+    
+    // Log both the actual client IP and the full list of IPs
+    console.log(`Actual Client IP: ${actualIP}`);
+    console.log(`Request passed through: ${ipList || 'No proxies involved'}`);
   
-  console.log(`Request received from IP: ${ip}`);
-  next();
-});
-
+    next();
+  });
+  
 app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
